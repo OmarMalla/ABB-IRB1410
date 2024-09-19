ebaf0de7f7f316ddb42c9c96765c1338002ea8a1
Omar Malla <92977092+OmarMalla@users.noreply.github.com>
2024-09-19 16:05:43 +0530

    Update IRB1410.m

--- IRB1410.m
+++ IRB1410.m
@@ -4,11 +4,10 @@ IRB1410 Simulation using the DH Apporach based on the simplified model
 and compared to the vlaues from IRB1410 Studio.
 
 Created as part of:
-Malla, O. and Shanmugavel, M. (2024),
-"Simplified model to study the kinematics of manipulators
-with parallelogram linkages", Industrial Robot,
-Vol. ahead-of-print No. ahead-of-print.
-https://doi.org/10.1108/IR-01-2024-0046 
+Malla, O. and Shanmugavel, M. (2024), 
+"Simplified model to study the kinematics of manipulators with parallelogram linkages", 
+Industrial Robot, Vol. 51 No. 5, pp. 704-714. 
+https://doi.org/10.1108/IR-01-2024-0046
 
 The code requires the use of Peter Corke's Robotics Toolbox for Matlab:
 https://petercorke.com/toolboxes/robotics-toolbox/
